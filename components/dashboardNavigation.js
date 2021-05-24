f9c15a82d8804f26a9cb2daf2e77ccc60885c9ac
Holland Pleskac <hollandpleskac@gmail.com>
2021-05-23 17:27:16 -0700

    dropdown

--- components/dashboardNavigation.js
+++ components/dashboardNavigation.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
@@ -42,7 +43,6 @@ const Sidebar = () => {
 
 const SidebarLink = ({ name, icon, href, spacingClasses }) => {
   const router = useRouter()
-  console.log(router.pathname)
   const activeRouteClasses = router.pathname === href ? 'text-white' : 'text-blue-100 hover:text-white'
   return (
     <Link href={href}>
@@ -56,11 +56,44 @@ const SidebarLink = ({ name, icon, href, spacingClasses }) => {
 
 
 const Topbar = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false)
+
+  const dropDownHandler = () => {
+    setDropdownOpen((prevState) => !prevState)
+  }
+
+
   return (
-    <div className='flex items-center justify-end pr-8 w-full h-topbar z-10 shadow cursor-pointer'>
-      <p className='mr-4 text-sm text-gray-500' >Holland Pleskac</p>
-      <img src="prof.jpg" alt="Prof Pic" width='45' height='45' className='rounded-full' />
-    </div>
+    <>
+      <div className='flex flex-col items-end justify-center pr-8 w-full h-topbar z-10 shadow'>
+        <div className='relative' >
+
+          <div onClick={dropDownHandler} className='flex items-center cursor-pointer' >
+            <p className='mr-4 text-sm text-gray-500' >Holland Pleskac</p>
+            <img src="prof.jpg" alt="Prof Pic" width='45' height='45' className='rounded-full' />
+          </div>
+
+          {isDropdownOpen && <div onClick={setDropdownOpen.bind(null, false)} className='fixed inset-0' ></div>}
+
+          {
+            isDropdownOpen &&
+            <div className='absolute right-0 flex flex-col w-48 py-2 mt-2 bg-white rounded-2xl shadow-xl' >
+              <button onClick={dropDownHandler} className='py-2 px-5 text-left text-gray-800 hover:bg-blue-600 hover:text-white focus:outline-none transition ease-in duration-100'>
+                Dark Mode
+            </button>
+              <button onClick={dropDownHandler} className='py-2 px-5 text-left text-gray-800 hover:bg-blue-600 hover:text-white focus:outline-none transition ease-in duration-100'>
+                Support
+            </button>
+              <button onClick={dropDownHandler} className='py-2 px-5 text-left text-gray-800 hover:bg-blue-600 hover:text-white focus:outline-none transition ease-in duration-100'>
+                Sign Out
+            </button>
+            </div>
+          }
+
+        </div>
+      </div>
+
+    </>
   )
 }
 
