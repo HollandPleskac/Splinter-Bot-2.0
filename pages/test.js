fd23f4594608586675619d807b5a342b4857c8bc
Holland Pleskac <hollandpleskac@gmail.com>
2021-06-02 13:44:47 -0700

    configuring charts (testing out)

--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
+
+// Line Chart
 
 const data = {
   labels: ['1', '2', '3', '4', '5', '6'],
@@ -27,10 +29,76 @@ const LineChart = () => (
         </a>
       </div>
     </div>
-    <div style={{ width: '500px', height: '100px' }} >
-      <Line data={data} />
+    <div className='mb-40' style={{ width: '500px', height: '100px' }} >
+      <Line data={data} options={{
+        plugins: {
+          title: {
+            display: true,
+            text: 'Testing',
+
+          },
+          legend: {
+            display: true,
+            position: 'right',
+            labels: {
+              pointStyle: 'circle',
+              usePointStyle: true
+            }
+          }
+        }
+      }} />
+    </div>
+    <div className='bg-gray-50' style={{ width: '500px', height: '220px' }}>
+      <BarChart />
     </div>
   </>
 );
 
+
+const BarChart = () => {
+  const data2 = {
+    labels: ['Fire', 'Water', 'Earth', 'Life'],
+
+    datasets: [
+      {
+        label: 'wins',
+        data: [0.5, 0.2, 0.3, 0.6],
+        backgroundColor: 'rgb(255, 99, 132)'
+      },
+      {
+        label: 'losses',
+        data: [0.5, 0.8, 0.7, 0.4],
+        backgroundColor: 'rgb(180, 140, 255)'
+      }
+    ]
+  }
+  return (
+    <>
+      <Bar
+        pointStyle="star"
+        data={data2}
+        options={{
+          maintainAspectRatio: false,
+
+          scales: {
+            x:
+            {
+              stacked: true,
+            }
+            ,
+            y:
+            {
+              stacked: true,
+            }
+          }
+        }}
+      />
+    </>
+  )
+}
+
+
+
 export default LineChart;
+
+// matches played and bar with win percentages (show numbers on bar too for accurate count)
