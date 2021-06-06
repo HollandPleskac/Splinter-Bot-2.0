af6f84e9eb82b0da075c8055a4de5c339b524e4e
Holland Pleskac <hollandpleskac@gmail.com>
2021-06-06 13:23:59 -0700

    t

--- pages/api/farm.js
+++ pages/api/farm.js
@@ -1,10 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
+async function setIsInMatch(matchStatus) {
+  await firebase.firestore().collection('Users').doc('dpleskac@gmail.com').update({
+    isInMatch: matchStatus
+  })
+}
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    await setIsInMatch(true)
     res.json({ result: 'success' })
   } else {
-    console.log('request not met')
+    res.json({ result: 'should be a post request' })
   }
 }
